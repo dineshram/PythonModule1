776e25eb0c86494cce459144d3ddf3d0e432260a
dineshram <PappuKutti08>
2017-11-01 20:17:56 +0100

    Example of using variables to calculate the circumference of a circle.

---
+++ FunWithVariables.py
@@ -0,0 +1,12 @@
+
+#this is a variable
+radius = 10
+
+#this is a constant
+myConstPi = 3.14
+
+print("The radius of the circle is ", radius)
+
+print("The circumference of a circle is :", 2*myConstPi*radius)
+
+print("The circumference of a circle as an integer is :", int(2*myConstPi*radius))
