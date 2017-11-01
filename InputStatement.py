8d6b1884cd279a23dd4f6e8ed2d3d9be4d95fffd
dineshram <PappuKutti08>
2017-11-01 20:34:31 +0100

    using user input to calculate the circumference of a circle

---
+++ InputStatement.py
@@ -0,0 +1,9 @@
+#here we want to take an input from the user
+
+print("Please enter the radius of the circle :")
+radius = float(input())
+print("You have entered a radius of", radius)
+
+myConstPi = 3.14
+
+print("The circumference of the circle is :", 2*myConstPi*radius)
