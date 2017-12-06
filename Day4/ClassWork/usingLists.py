2c81aeef6a0a44a00230df2d819edb60bcd28213
dineshram <PappuKutti08>
2017-12-06 18:24:19 +0100

    Introduction to lists.

---
+++ Day4/ClassWork/usingLists.py
@@ -0,0 +1,29 @@
+
+
+myList = [1, 2, 3, 5, 8, 13, 21]
+
+print(myList)
+print(myList[0])
+print(myList[4])
+#print(myList[5])
+
+#defining the function
+def printMyList():
+    print("In the function")
+    for i in range(0, len(myList)):
+        print(myList[i])
+
+#calling the function
+printMyList()
+
+
+def addMyList():
+    sumOfList = 0
+    print("Summing up my list of numbers")
+    for i in range(0, len(myList)):
+        sumOfList = sumOfList + myList[i]
+
+    print("The sum of myList is ", sumOfList)
+
+addMyList()
+
